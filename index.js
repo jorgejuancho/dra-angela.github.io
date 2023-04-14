e894b541aa29df40f40dec93a2a1211f54d39aff
jorge juan dominguez <jorgejuandominguez@gmail.com>
2023-04-14 17:43:42 +0200

    añado hoja de estilos y js con menu tipo hamburguesa

---
+++ index.js
@@ -0,0 +1,8 @@
+import hamburguerMenu from "./menu_hamburguesa.js";
+
+const d = document;
+
+d.addEventListener("DOMContentLoaded", e =>{
+    hamburguerMenu(".panel-btn",".panel", ".menu a")
+
+});
